5d897ff5fda083cfb85961c07d3dc42da0b0c27e
Shaky-Heart <104614648+Shaky-Heart@users.noreply.github.com>
2023-09-03 18:02:36 +0800

    Add files via upload
    Added two basic layout files for the main activity and the main menu fragment

---
+++ MAD_Assignment-main/app/src/main/java/com/example/myapplication/Game.java
@@ -0,0 +1,235 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    //private final static int player1 = 1;
+    //private final static int player2 = 2;
+    private int turn = 1;
+    private int winLength;
+    private int[][] board;
+    private int boardDimensions;
+    private List<int[]> moveList;  
+    
+
+    public Game(int size, int winCondition) {
+        if(!legalCoord(size)) {
+            //Chuck an error
+        }
+        board = makeBoard(size);
+        boardDimensions = size;
+        moveList = new ArrayList<int[]>();
+        winLength = winCondition;
+    }
+
+    public int[][] makeBoard(int size) {
+
+        int[][] board = new int[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                board[i][j] = 0;
+            }
+        }
+        
+        return board;
+    }
+
+    public boolean legalCoord(int size) {
+        if(size > 6) {
+            return false;
+        }
+        else if(size < 2) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    public boolean legalMove(int xCoord, int yCoord) {
+        if(!legalCoord(yCoord)) {
+            return false;
+        }
+        if(!legalCoord(xCoord)) {
+            return false;
+        } 
+        if(board[yCoord][xCoord] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public void changeTurn() {
+        if(turn == 1) {
+            turn = 2;
+        }
+        else {
+            turn = 1;
+        }
+    }
+
+    public int makeMove(int xCoord, int yCoord) {
+        int infoReturn = -1;
+        
+        if(legalMove(xCoord, yCoord)) {
+            
+            board[yCoord][xCoord] = turn;
+
+            if(hasWon(xCoord, yCoord)) {
+                infoReturn = 1;
+            }
+            else if(hasDraw()) {
+                infoReturn = 2;
+            }
+            else {
+                infoReturn = 0;
+            }
+            int[] move = {xCoord, yCoord};
+            moveList.add(move);
+            changeTurn();
+        }
+
+        return infoReturn;
+    }
+    private int movesMade() {
+        return moveList.size();
+    }
+
+    private int movesLeft() {
+        return (boardDimensions * boardDimensions - movesMade());
+    }
+    
+    public void resetGame() {
+        board = makeBoard(boardDimensions);
+        moveList = new ArrayList<int[]>();
+        turn = 1;
+    }
+    public void undoLastMove() {
+        if(moveList.size() != 0) {
+            int[] move = moveList.remove(moveList.size() - 1);
+            board[move[1]][move[0]] = 0;
+            changeTurn();
+        }
+        //Maybe have it throw an error but nothing is ok for now
+    }
+
+    public boolean hasWon(int xCoord, int yCoord) {
+        if(TleftTobRightDiagonal(xCoord, yCoord)) {
+            return true;
+        }
+        else if(tRightTobLeftDiagonal(xCoord, yCoord)) {
+            return true;
+        }
+        else if(topToBottom(xCoord, yCoord)) {
+            return true;
+        }
+        else if(leftToRight(xCoord, yCoord)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasDraw() {
+        for(int i = 0; i < boardDimensions; i++) {
+            for(int j = 0; j < boardDimensions; j++) {
+                if(board[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean TleftTobRightDiagonal(int xCoord, int yCoord) {
+        int i = 0;
+        int j = xCoord - yCoord;
+        int consecutiveTokens = 0;
+        
+        if(j < 0) {
+            i = 0 - j;
+            j = 0;
+        }
+        while(i < boardDimensions)
+        {
+            if(board[j][i] != turn) {
+                consecutiveTokens = 0;
+            }
+            else {
+                consecutiveTokens++;
+            }    
+            i++;
+        }
+        
+        if(consecutiveTokens >= winLength) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean tRightTobLeftDiagonal(int xCoord, int yCoord) {
+        int consecutiveTokens = 0;
+        int i = 0;
+        int j = xCoord + yCoord;
+        if(j > (boardDimensions - 1)) {
+            i = j - boardDimensions - 1; 
+            j = boardDimensions - 1;
+        } 
+    
+        while(i >= 0 && j < boardDimensions)
+        {
+            if(board[j][i] != turn) {
+                consecutiveTokens = 0;
+            }
+            else {
+                consecutiveTokens++;
+            }    
+            i--;
+            j++;
+        }
+        
+        if(consecutiveTokens >= winLength) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean topToBottom(int xCoord, int yCoord) {
+        int consecutiveTokens = 0;
+        for(int i = 0; i < boardDimensions; i++) {
+            if(board[i][xCoord] != turn) {
+                consecutiveTokens = 0;
+            }
+            else {
+                consecutiveTokens++;
+            }
+        }
+        
+        if(consecutiveTokens >= winLength) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean leftToRight(int xCoord, int yCoord) {
+        int consecutiveTokens = 0;  
+        for(int i = 0; i < boardDimensions; i++) {
+            if(board[yCoord][i] != turn) {
+                consecutiveTokens = 0;
+            }
+            else {
+                consecutiveTokens++;
+            }
+        }
+        
+        if(consecutiveTokens >= winLength) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ MAD_Assignment-main/app/src/main/java/com/example/myapplication/MenuFragment.java
@@ -0,0 +1,50 @@
+/*
+    Menu Fragment created by Caio Stirling
+    Will handle menu operations and pass it on to the main activity
+ */
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MenuFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuFragment extends Fragment {
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Implement a menu function within this fragment
+
+    public static MenuFragment newInstance(String param1, String param2) {
+        MenuFragment fragment = new MenuFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
